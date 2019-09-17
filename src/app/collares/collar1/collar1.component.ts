afee16ec1ff92485618d0888aa4f5a65d1fd8e2a
AEDD1970 <ing.alexis.duque@gmail.com>
2019-09-17 00:40:45 -0500

    actulizacion para mariana

--- src/app/collares/collar1/collar1.component.ts
+++ src/app/collares/collar1/collar1.component.ts
@@ -1,73 +1,82 @@
-import { Component, OnInit, Input, OnDestroy, ViewChild, TemplateRef } from '@angular/core';
-import { Product, ProductApi } from '../../models/producto';
-import { PersonaService } from 'src/app/servicio/persona.service';
-import { Subscription } from 'rxjs';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnDestroy,
+  ViewChild,
+  TemplateRef
+} from "@angular/core";
+import { Product, ProductApi } from "../../models/producto";
+import { PersonaService } from "src/app/servicio/persona.service";
+import { Subscription } from "rxjs";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-collar1',
-  templateUrl: './collar1.component.html',
-  styleUrls: ['./collar1.component.css']
+  selector: "app-collar1",
+  templateUrl: "./collar1.component.html",
+  styleUrls: ["./collar1.component.css"]
 })
-
-
-
 export class Collar1Component implements OnInit, OnDestroy {
-  @ViewChild('modalForms', {read:'',static: true})
+  @ViewChild("modalForms", { read: "", static: true })
+  modalForms: TemplateRef<HTMLElement>;
 
-  agregarTypeProducto: Product = { id: '', producname: '', type_product_id: '', price: '', description: '', quantiyy: '', image: '' }
-  typec: ProductApi
   @Input() reciveData;
-  private unsuscribe: Subscription;
-  
 
+  private unsuscribe: Subscription;
+  private typeProduct: Product = {
+    id: "",
+    producname: "",
+    type_product_id: "",
+    price: "",
+    description: "",
+    quantiyy: "",
+    image: ""
+  };
+  typec: ProductApi;
   public objectOption: any = [];
-  public quantity:number = 0;
-  
-  
-  
-  constructor(private personaService: PersonaService, public ngbModal:NgbModal) {
+  public quantity: number = 0;
+
+  constructor(
+    private personaService: PersonaService,
+    public ngbModal: NgbModal
+  ) {
+    console.log(this);
   }
   ngOnInit() {
-   this.unsuscribe = this.reciveData.subscribe(data => {
-      this.typec=data
-      this.objectOption=[];
+    this.unsuscribe = this.reciveData.subscribe(data => {
+      this.typec = data;
+      this.objectOption = [];
       for (let index = 0; index < this.typec.quantiyy; index++) {
-        this.objectOption.push({value:index+1,option:(index+1).toString()+' units'})
-        
+        this.objectOption.push({
+          value: index + 1,
+          option: (index + 1).toString() + " units"
+        });
       }
 
-      console.log( this.objectOption);
-    })
-   
+      console.log(this.objectOption);
+    });
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.unsuscribe.unsubscribe();
   }
 
   //obtener los collares
   ObetenerTypec(id) {
-    this.personaService.obtenerTodosTypeCollar(id).subscribe(resultado => {
-     
-      this.typec =resultado;
-
-     
-    },
+    this.personaService.obtenerTodosTypeCollar(id).subscribe(
+      resultado => {
+        this.typec = resultado;
+      },
       error => {
-
         console.log(JSON.stringify(error));
-      });
-
+      }
+    );
   }
 
-  changeCountry(){
-console.log(this.quantity)
+  changeCountry() {
+    console.log(this.quantity);
   }
 
-
-  openMoldal(content) {
-    this.ngbModal.open(content, { windowClass: 'dark-modal' });
+  openMoldal() {
+    this.ngbModal.open(this.modalForms, { windowClass: "dark-modal" });
   }
-
-
 }
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,32 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"]
 })
 export class IndexComponent implements OnInit {
-  public index = 0
-  public timeOut = 5500
+  @ViewChild("modalForms", { read: "", static: true }) public modalForms;
 
-  public images = [
-    'img_la.jpg',
-    'lili-blanca.jpg',
-    'rosa-negra.jpg',
-  ]
-  public logo = [
-    'logo.PNG'
-  ]
+  public index = 0;
+  public timeOut = 5500;
+  public images = ["img_la.jpg", "lili-blanca.jpg", "rosa-negra.jpg"];
+  public logo = ["logo.PNG"];
 
-  constructor() { }
+  constructor(public NgbModal: NgbModal) {}
 
   ngOnInit() {
     const carousel = () => {
+      
       this.index++;
-      if (this.index >= this.images.length) { this.index = 0 }
+      if (this.index >= this.images.length) {
+        this.index = 0;
+      }
       setTimeout(carousel, this.timeOut); // Change image every 2 seconds
-    }
+    };
     carousel();
   }
 
+  openRegister() {
+    this.NgbModal.open(this.modalForms, { windowClass: "dark-modal" });
+  }
 }
